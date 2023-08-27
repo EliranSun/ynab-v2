47c070807aaf8b2732d4ebf30c8332ad30a3161c
Eliran Shemesh <piro27@gmail.com>
2023-08-27 19:04:50 +0300

    hebrew layout

--- src/components/molecules/PastTwelveMonthsBalance/PastTwelveMonthsBalance.jsx
+++ src/components/molecules/PastTwelveMonthsBalance/PastTwelveMonthsBalance.jsx
@@ -1,14 +1,17 @@
-import { Title } from "../../atoms";
+import { Button, Title } from "../../atoms";
 import { formatCurrency } from "../../../utils";
-import { useContext, useMemo } from "react";
+import { useContext, useMemo, useState } from "react";
 import { isAfter, subMonths } from "date-fns";
 import { ExpensesContext } from "../../../context";
 import { useBudget } from "../../../hooks/useBudget";
 import { Trans } from "@lingui/macro";
+import { Calendar } from "@phosphor-icons/react";
+import { noop } from "lodash";
 
 const IncomeSubcategoryIds = [80, 81, 82, 83];
 
-export const PastTwelveMonthsBalance = ({ timestamp }) => {
+export const PastTwelveMonthsBalance = ({ timestamp, onViewToggle = noop }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const { expensesArray: expenses = [] } = useContext(ExpensesContext);
   const { income: incomeBudget, outcome: expensesBudget } = useBudget(timestamp);
   
@@ -32,47 +35,52 @@ export const PastTwelveMonthsBalance = ({ timestamp }) => {
   const budgetBottomLine = incomeBudget * 12 - expensesBudget * 12;
   
   return (
-    <div className="text-xl font-bold bg-gray-100 p-4">
-      <Title type={Title.Types.H2}>
-        <Trans>
-          Past 12 months
-        </Trans>
-      </Title>
-      <p className="font-normal italic">
-        <Trans>
-          These are the incomes and expenses over the last 12 months.
-          Put in mind though - the budget is for the current month only,
-          so some months (i.e. vacation) are skewing these results.
-        </Trans>
-      </p>
-      <div className="text-base flex gap-4">
-        <div>
-          <Trans>Actual:</Trans><br/>
-          <span className="text-green-500">{formatCurrency(totalIncome)}</span> - <br/>
-          <span className="text-red-500">{formatCurrency(totalExpenses)}</span> = <br/>
-          <hr/>
-          {formatCurrency(actualBottomLine)}
+    <div className="m-4">
+      <Button type={Button.Types.GHOST_BORDERED}>
+        <Calendar size={24} onClick={() => {
+          setIsOpen(!isOpen);
+          onViewToggle();
+        }} color="black"/>
+      </Button>
+      {isOpen && <div className="text-xl font-bold bg-gray-100">
+        <Title type={Title.Types.H3} className="mb-4">
+          <Trans>
+            Past 12 months
+          </Trans>
+        </Title>
+        <p className="text-sm italic mb-4 font-light">
+          <Trans>
+            Put in mind though - the budget is for the current month only,
+            so some months (i.e. vacation) are skewing these results.
+          </Trans>
+        </p>
+        <div className="text-lg flex gap-4 mb-4">
+          <div>
+            <Trans>Budget:</Trans> <br/>
+            <span className="text-green-500">{formatCurrency(incomeBudget * 12)}</span> - <br/>
+            <span className="text-red-500">{formatCurrency(expensesBudget * 12)}</span> = <br/>
+            <hr/>
+            {formatCurrency(budgetBottomLine)}
+          </div>
+          <div>
+            <Trans>Actual:</Trans><br/>
+            <span className="text-green-500">{formatCurrency(totalIncome)}</span> - <br/>
+            <span className="text-red-500">{formatCurrency(totalExpenses)}</span> = <br/>
+            <hr/>
+            {formatCurrency(actualBottomLine)}
+          </div>
+          <div>
+            <Trans>Diffs:</Trans> <br/>
+            <span className="text-green-500">
+              {formatCurrency(totalIncome - (incomeBudget * 12))}
+            </span> - <br/>
+            <span className="text-red-500">
+              {formatCurrency(totalExpenses - (expensesBudget * 12))}</span> = <br/>
+            <hr/>
+            {formatCurrency(actualBottomLine - budgetBottomLine)}
+          </div>
         </div>
-        
-        <div>
-          <Trans>Budget:</Trans> <br/>
-          <span className="text-green-500">{formatCurrency(incomeBudget * 12)}</span> - <br/>
-          <span className="text-red-500">{formatCurrency(expensesBudget * 12)}</span> = <br/>
-          <hr/>
-          {formatCurrency(budgetBottomLine)}
-        </div>
-        
-        <div>
-          <Trans>Diffs:</Trans> <br/>
-          <span className="text-green-500">
-            {formatCurrency(totalIncome - (incomeBudget * 12))}
-          </span> - <br/>
-          <span className="text-red-500">
-            {formatCurrency(totalExpenses - (expensesBudget * 12))}</span> = <br/>
-          <hr/>
-          {formatCurrency(actualBottomLine - budgetBottomLine)}
-        </div>
-      </div>
+      </div>}
     </div>
   )
 };
--- src/components/pages/BalanceView/BalanceView.jsx
+++ src/components/pages/BalanceView/BalanceView.jsx
@@ -1,9 +1,9 @@
+import { useContext, useState } from "react";
 import { useDate } from "../../molecules";
 import { Title } from "../../atoms";
 import { CategoryBalance } from "./CategoryBalance";
 import { ThisMonthBalance } from "../../molecules/ThisMonthBalance/ThisMonthBalance";
 import { PastTwelveMonthsBalance } from "../../molecules/PastTwelveMonthsBalance/PastTwelveMonthsBalance";
-import { useContext } from "react";
 import { ExpensesContext } from "../../../context";
 import { useBudget } from "../../../hooks/useBudget";
 import { useExpensesSummary } from "../../../hooks/useExpensesSummary";
@@ -12,6 +12,7 @@ import { Trans } from "@lingui/macro";
 const isDesktop = window.innerWidth > 768;
 
 const BalanceView = () => {
+    const [isPastTwelveMonthsOpen, setIsPastTwelveMonthsOpen] = useState(false);
     const { categoriesByAmount } = useContext(ExpensesContext);
     const { currentTimestamp, NextButton, PreviousButton, isSameDate, isPreviousMonth } = useDate();
     
@@ -20,26 +21,31 @@ const BalanceView = () => {
     
     return (
       <section className="h-[90vh] overflow-y-auto overflow-x-hidden">
-        {isDesktop && <div className="p-4">
-          <Title className="mb-4">
-            <Trans>BALANCE & BUDGET</Trans>
-          </Title>
-          <ul className="w-full bg-gray-100 w-full h-10 my-2 flex">
-            <li className="bg-blue-900 w-1/3 h-full"></li>
-            <li className="bg-red-900 w-1/3 h-full"></li>
-            <li className="bg-green-900 w-1/3 h-full"></li>
-          </ul>
-        </div>}
-        <PastTwelveMonthsBalance timestamp={currentTimestamp}/>
-        <div className="flex gap-2 md:my-4 items-center w-full justify-evenly sticky top-0 bg-white p-4 z-10">
-          <PreviousButton/>
-          <Title type={Title.Types.H2} className="font-sans font-black">
-            {new Date(currentTimestamp).toLocaleString("en-US", {
-              month: "short",
-              year: "2-digit",
-            })}
-          </Title>
-          <NextButton/>
+        {isDesktop &&
+          <div className="p-4">
+            <Title className="mb-4">
+              <Trans>BALANCE & BUDGET</Trans>
+            </Title>
+            <ul className="w-full bg-gray-100 w-full h-10 my-2 flex">
+              <li className="bg-blue-900 w-1/3 h-full"></li>
+              <li className="bg-red-900 w-1/3 h-full"></li>
+              <li className="bg-green-900 w-1/3 h-full"></li>
+            </ul>
+          </div>}
+        <div className="flex">
+          <PastTwelveMonthsBalance
+            onViewToggle={() => setIsPastTwelveMonthsOpen(!isPastTwelveMonthsOpen)}
+            timestamp={currentTimestamp}/>
+          <div className="flex gap-2 md:my-4 items-center w-full justify-evenly sticky top-0 bg-white p-4 z-10">
+            <PreviousButton/>
+            <Title type={Title.Types.H2} className="font-sans font-black">
+              {new Date(currentTimestamp).toLocaleString("he-IL", {
+                month: "short",
+                year: "2-digit",
+              })}
+            </Title>
+            <NextButton/>
+          </div>
         </div>
         <ThisMonthBalance
           incomeBudget={incomeBudget}

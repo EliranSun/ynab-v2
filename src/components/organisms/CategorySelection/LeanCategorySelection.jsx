9f42b2106d7f84b1fdf71e230fa1431648d368c6
Eliran Shemesh <elirans@steps.me>
2023-10-11 14:24:33 +0300

    Translate category selection

--- src/components/organisms/CategorySelection/LeanCategorySelection.jsx
+++ src/components/organisms/CategorySelection/LeanCategorySelection.jsx
@@ -8,7 +8,9 @@ const LeanCategorySelection = ({ onCategorySelect = noop }) => {
       {Categories.map((category) => {
         return (
           <div key={category.id} className="flex flex-col bg-gray-100 w-full">
-            <div className="p-4 bg-gray-500 text-white"><b>{category.name}</b></div>
+            <div className="p-4 bg-gray-500 text-white">
+              <b>{category.label}</b>
+            </div>
             <div className="">
               {category.subCategories.map((sub) => {
                 return (
@@ -17,7 +19,7 @@ const LeanCategorySelection = ({ onCategorySelect = noop }) => {
                     className={classNames("flex gap-2 py-2 px-4 text-sm hover:bg-red-500 hover:text-white cursor-pointer hover:border-black-300")}
                     onClick={() => onCategorySelect(sub.id)}>
                     <span>{sub.icon.slice(0, 2)}</span>
-                    <span>{sub.name}</span>
+                    <span>{sub.label}</span>
                   </div>
                 );
               })}

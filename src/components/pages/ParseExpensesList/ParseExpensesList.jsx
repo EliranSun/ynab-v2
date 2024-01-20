04847991912082bcc75e13f2b997aee8c99c29e8
Eliran Shemesh <piro27@gmail.com>
2024-01-20 16:34:11 +0200

    Fix no expenses return on new paste

--- src/components/pages/ParseExpensesList/ParseExpensesList.jsx
+++ src/components/pages/ParseExpensesList/ParseExpensesList.jsx
@@ -11,126 +11,128 @@ import { ClipboardText } from "@phosphor-icons/react";
 const isMobile = window.innerWidth < 768;
 
 export const ParseExpensesList = ({
-  text = "", expenses, setExpenses = () => {
-  }
+    text = "", expenses, setExpenses = () => {
+    }
 }) => {
-  const textAreaRef = useRef(null);
-  const [message, setMessage] = useState("");
-  const [value, setValue] = useState(text);
-  const [isParseButtonDisabled, setIsParseButtonDisabled] = useState(false);
-  const [parsedExpenses, setParsedExpenses] = useState([]);
-  const [isStatusAnimated, setIsStatusAnimated] = useState(false);
+    const textAreaRef = useRef(null);
+    const [message, setMessage] = useState("");
+    const [value, setValue] = useState(text);
+    const [isParseButtonDisabled, setIsParseButtonDisabled] = useState(false);
+    const [parsedExpenses, setParsedExpenses] = useState([]);
+    const [isStatusAnimated, setIsStatusAnimated] = useState(false);
 
 
-  useEffect(() => {
-    if (isStatusAnimated) {
-      setTimeout(() => {
-        setIsStatusAnimated(false);
-      }, 3000);
+    useEffect(() => {
+        if (isStatusAnimated) {
+            setTimeout(() => {
+                setIsStatusAnimated(false);
+            }, 3000);
+        }
+    }, [parsedExpenses, isStatusAnimated]);
+
+    useEffect(() => {
+        const savedTextareaValue = localStorage.getItem("textarea-value");
+        const savedParsedExpenses = localStorage.getItem("parsed-expenses");
+
+        if (savedParsedExpenses) {
+            const data = JSON.parse(savedParsedExpenses);
+            const nonExistingExpenses = data.filter(item => {
+                return !item.categoryId;
+            });
+
+            setParsedExpenses(nonExistingExpenses.map(item => new Expense(item)));
+        }
+
+        if (savedTextareaValue) {
+            setValue(savedTextareaValue);
+        }
+    }, [expenses]);
+
+    useEffect(() => {
+        if (!value) {
+            return;
+        }
+
+        localStorage.setItem("textarea-value", value);
+    }, [value]);
+
+    useEffect(() => {
+        if (!parsedExpenses.length) {
+            return;
+        }
+
+        localStorage.setItem("parsed-expenses", JSON.stringify(parsedExpenses));
+    }, [parsedExpenses]);
+
+    const setNewExpenses = () => {
+        if (!textAreaRef.current) {
+            return;
+        }
+
+        setIsStatusAnimated(true);
+        const newExpenses = parseNewExpenses(textAreaRef.current.value, expenses);
+
+        if (newExpenses.length === 0) {
+            setMessage("Nothing new");
+            return;
+        }
+
+        setMessage(`${newExpenses.length} new expenses`);
+        setParsedExpenses(newExpenses);
+        localStorage.setItem("parsed-expenses", JSON.stringify(newExpenses));
     }
-  }, [parsedExpenses, isStatusAnimated]);
 
-  useEffect(() => {
-    const savedTextareaValue = localStorage.getItem("textarea-value");
-    const savedParsedExpenses = localStorage.getItem("parsed-expenses");
+    return (
+        <section className="h-screen overflow-y-auto p-4">
+            <Title type={Title.Types.H1} className="flex items-center gap-2 mb-4">
+                <ClipboardText size={50}/> Parse
+            </Title>
 
-    if (savedParsedExpenses) {
-      const data = JSON.parse(savedParsedExpenses);
-      setParsedExpenses(data.filter(item => {
-        return !isExistingExpense(item, expenses);
-      }).map(item => new Expense(item)));
-    }
-
-    if (savedTextareaValue) {
-      setValue(savedTextareaValue);
-    }
-  }, [expenses]);
-
-  useEffect(() => {
-    if (!value) {
-      return;
-    }
-
-    localStorage.setItem("textarea-value", value);
-  }, [value]);
-
-  useEffect(() => {
-    if (!parsedExpenses.length) {
-      return;
-    }
-
-    localStorage.setItem("parsed-expenses", JSON.stringify(parsedExpenses));
-  }, [parsedExpenses]);
-
-  const setNewExpenses = () => {
-    if (!textAreaRef.current) {
-      return;
-    }
-
-    setIsStatusAnimated(true);
-    const newExpenses = parseNewExpenses(textAreaRef.current.value, expenses);
-
-    if (newExpenses.length === 0) {
-      setMessage("Nothing new");
-      return;
-    }
-
-    setMessage(`${newExpenses.length} new expenses`);
-    setParsedExpenses(newExpenses);
-    localStorage.setItem("parsed-expenses", JSON.stringify(newExpenses));
-  }
-
-  return (
-    <section className="h-screen overflow-y-auto p-4">
-      <Title type={Title.Types.H1} className="flex items-center gap-2 mb-4">
-        <ClipboardText size={50}/> Parse
-      </Title>
-
-      <div className="max-w-6xl m-auto">
-        {parsedExpenses.length > 0 &&
-          <Title type={isMobile ? Title.Types.H3 : Title.Types.H2}>
-            Existing expenses: {expenses.length} • New expenses: ${parsedExpenses.length}
-          </Title>}
-        <ExpensesList
-          expenses={parsedExpenses}
-          existingExpenses={expenses}
-          setExpenses={setParsedExpenses}
-          submitExpenses={setExpenses}/>
-      </div>
-      <div className="flex items-start gap-4 max-w-7xl flex-col">
-        <div className="flex flex-col md:w-1/3 h-full">
-          <Title type={Title.Types.H2} className="mb-4">Paste</Title>
-          <textarea
-            className="border border-dashed border-black p-4 outline-none w-full h-full"
-            placeholder="Paste expenses here"
-            rows={isMobile ? 5 : 15}
-            ref={textAreaRef}
-            value={value}
-            onChange={event => {
-              setIsParseButtonDisabled(!event.target.value);
-              setValue(event.target.value);
-            }}/>
-        </div>
-        <div className="mb-4">
-          <Title type={Title.Types.H2} className="mb-4">Upload</Title>
-          <SheetUpload onSheetParse={data => console.log(data)}/>
-        </div>
-        <div className="">
-          <Title type={Title.Types.H2} className="mb-4">Manual</Title>
-          WIP
-        </div>
-        <Button
-          size={Button.Sizes.FULL}
-          isDisabled={isParseButtonDisabled}
-          onClick={setNewExpenses}
-          className={classNames("my-4 w-72 mx-auto text-center", {
-            "animate-pulse duration-500": isStatusAnimated,
-          })}>
-          {isStatusAnimated ? message : "Parse expenses"}
-        </Button>
-      </div>
-    </section>
-  );
+            <div className="max-w-6xl m-auto">
+                {parsedExpenses.length > 0 &&
+                    <Title type={isMobile ? Title.Types.H3 : Title.Types.H2}>
+                        Existing expenses: {expenses.length} • New expenses: {parsedExpenses.length}
+                    </Title>}
+                <ExpensesList
+                    expenses={parsedExpenses}
+                    existingExpenses={expenses}
+                    setExpenses={setParsedExpenses}
+                    submitExpenses={setExpenses}/>
+            </div>
+            <div className="flex items-start gap-4 max-w-7xl flex-col">
+                <div className="flex flex-col md:w-1/3 h-full">
+                    <Title type={Title.Types.H2} className="mb-4">Paste</Title>
+                    <textarea
+                        className="border border-dashed border-black p-4 outline-none w-full h-full"
+                        placeholder="Paste expenses here"
+                        rows={isMobile ? 5 : 15}
+                        ref={textAreaRef}
+                        value={value}
+                        onChange={event => {
+                            setIsParseButtonDisabled(!event.target.value);
+                            setValue(event.target.value);
+                        }}/>
+                </div>
+                <div className="mb-4">
+                    <Title type={Title.Types.H2} className="mb-4">Upload</Title>
+                    <SheetUpload onSheetParse={data => console.log(data)}/>
+                </div>
+                <div className="">
+                    <Title type={Title.Types.H2} className="mb-4">Manual</Title>
+                    WIP
+                </div>
+                <Button
+                    size={Button.Sizes.FULL}
+                    isDisabled={isParseButtonDisabled}
+                    onClick={setNewExpenses}
+                    className={classNames("my-4 w-72 mx-auto text-center", {
+                        "animate-pulse duration-500": isStatusAnimated,
+                    })}>
+                    {isStatusAnimated ? message : "Parse expenses"}
+                </Button>
+            </div>
+        </section>
+    );
 };
 
 export default withExpensesContext(ParseExpensesList);
--- src/utils/expenses.js
+++ src/utils/expenses.js
@@ -1,56 +1,58 @@
 import { Expense } from "../models";
 
 export const isExistingExpense = (newExpense, expenses) => {
-  return expenses.find((expense) => {
-    return (
-        expense.name === newExpense.name &&
-        expense.timestamp === newExpense.timestamp &&
-        newExpense.amount === expense.amount
-    );
-  });
+    const existingExpense = expenses.find((expense) => {
+        return (
+            expense.name === newExpense.name &&
+            expense.timestamp === newExpense.timestamp &&
+            newExpense.amount === expense.amount
+        );
+    });
+
+    return !!existingExpense;
 };
 
 export const getExistingExpenses = (expense, expenses) => {
-  return expenses.filter((item) => {
-    return (
-        expense.name === item.name &&
-        expense.timestamp === item.timestamp &&
-        expense.amount === item.amount
-    );
-  });
+    return expenses.filter((item) => {
+        return (
+            expense.name === item.name &&
+            expense.timestamp === item.timestamp &&
+            expense.amount === item.amount
+        );
+    });
 };
 
 export const parseNewExpenses = (text = '', existingExpenses = []) => {
-  const rows = text.split("\n");
-  return rows.map((row) => {
-    const cells = row.split("\t");
-    const name = cells[0];
-    const amount = cells[4];
-    const dateParts = cells[1]?.split("/");
-    const year = dateParts && `20${dateParts[2]}`;
-    const month = dateParts && Number(dateParts[1]) - 1;
-    const day = dateParts && dateParts[0];
-    const timestamp = new Date(Date.UTC(year, month, day)).getTime();
-    const parsedAmount =
-        amount &&
-        parseFloat(amount.replace(",", "").replace("₪", "").trim());
-    
-    if (!name || !parsedAmount || !timestamp) {
-      return {};
-    }
-    
-    return new Expense({
-      name: name,
-      timestamp: timestamp,
-      amount: parsedAmount,
-      note: cells[5],
-    });
-  })
-      .filter((row) => {
-        if (isExistingExpense(row, existingExpenses)) {
-          return false;
+    const rows = text.split("\n");
+    return rows.map((row) => {
+        const cells = row.split("\t");
+        const name = cells[0];
+        const amount = cells[4];
+        const dateParts = cells[1]?.split("/");
+        const year = dateParts && `20${dateParts[2]}`;
+        const month = dateParts && Number(dateParts[1]) - 1;
+        const day = dateParts && dateParts[0];
+        const timestamp = new Date(Date.UTC(year, month, day)).getTime();
+        const parsedAmount =
+            amount &&
+            parseFloat(amount.replace(",", "").replace("₪", "").trim());
+
+        if (!name || !parsedAmount || !timestamp) {
+            return {};
         }
-        
-        return row.name && row.amount && row.timestamp;
-      });
+
+        return new Expense({
+            name: name,
+            timestamp: timestamp,
+            amount: parsedAmount,
+            note: cells[5],
+        });
+    })
+        .filter((row) => {
+            if (isExistingExpense(row, existingExpenses)) {
+                return false;
+            }
+
+            return row.name && row.amount && row.timestamp;
+        });
 };

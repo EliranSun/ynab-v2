093347c859d0be87258a1372ce2c401c55bfabcd
Eliran Shemesh <piro27@gmail.com>
2024-02-11 11:09:12 +0200

    Negative/positive budget deviation indication

--- src/components/pages/BalanceView/CategoryBalance.jsx
+++ src/components/pages/BalanceView/CategoryBalance.jsx
@@ -1,12 +1,15 @@
 import {useContext, useMemo, useState} from "react";
 import {orderBy} from "lodash";
 import {isSameMonth} from "date-fns";
+import {Smiley, SmileySad} from "@phosphor-icons/react";
 import {Categories} from "../../../constants";
 import {formatCurrency} from "../../../utils";
 import Subcategory from "./Subcategory";
 import {BudgetContext, ExpensesContext, getDateKey} from "../../../context";
 import {useCategoryExpensesSummary} from "../../../hooks/useCategoryExpensesSummary";
+import classNames from "classnames";
 
+const INCOME_CATEGORY_ID = 8;
 export const CategoryBalance = ({
                                     categoryId,
                                     categoryName,
@@ -64,15 +67,31 @@ export const CategoryBalance = ({
         return null;
     }
 
+    const diff = categoryBudget - totalExpensesSum;
+    const isIncome = categoryId === INCOME_CATEGORY_ID;
+
     return (
-        <div className="bg-gray-200 px-2 pb-4 w-full max-w-xl box-border">
-            <div className="flex justify-between gap-2 md:gap-4 my-2 text-sm md:text-xl font-bold"
-                 onClick={() => setIsExpanded(!isExpanded)}>
+        <div className="bg-gray-200 p-2 md:p-4 w-full max-w-2xl box-border relative">
+            <div
+                className="flex items-center justify-between gap-2 md:gap-4 my-2 text-sm md:text-xl font-bold"
+                onClick={() => setIsExpanded(!isExpanded)}>
                 <span className="w-1/4">{categoryName}</span>
-                <div className="flex gap-4">
-                    <span className="">{formatCurrency(totalExpensesSum, false, false)}</span>
-                    <span className="">{formatCurrency(categoryBudget, false, false)}</span>
+                <div className="flex gap-4 w-2/3 md:w-1/2 text-right items-center">
+                    <span className="w-1/3">{formatCurrency(totalExpensesSum, false, false)}</span>
+                    <span className="w-1/3">{formatCurrency(categoryBudget, false, false)}</span>
+                    <span className={classNames("w-1/3 flex items-center gap-2 justify-end", {
+                        "text-green-500": diff > 0,
+                        "text-red-500": diff < 0
+                    })}>
+                        {!isIncome ? formatCurrency(diff) : null}
+                    </span>
                 </div>
+            </div>
+            <div className={classNames("hidden md:inline absolute -right-6 top-0 bottom-0 m-auto", {
+                "text-green-500": diff > 0,
+                "text-red-500": diff < 0
+            })}>
+                {!isIncome ? diff > 0 ? <Smiley/> : <SmileySad/> : null}
             </div>
             {isExpanded ?
                 <div className="flex gap-2 min-w-fit flex-wrap items-stretch">

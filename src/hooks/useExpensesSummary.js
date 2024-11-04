b4397377a4adffdd0455054cd706df16571d02da
Eliran Shemesh <piro27@gmail.com>
2024-11-04 12:13:10 +0200

    Update useExpensesSummary.js

--- src/hooks/useExpensesSummary.js
+++ src/hooks/useExpensesSummary.js
@@ -4,7 +4,8 @@ import {ExpensesContext} from "../context";
 
 export const useExpensesSummary = (timestamp) => {
     const {expenses = []} = useContext(ExpensesContext);
-
+    const categories = useCategories(timestamp);
+    
     const totalExpensesThisMonth = useMemo(() => {
         return expenses.reduce((acc, curr) => {
             const isExpenseThisMonth = isSameMonth(new Date(curr.timestamp), new Date(timestamp));
@@ -22,9 +23,30 @@ export const useExpensesSummary = (timestamp) => {
             return acc + curr.amount;
         }, 0);
     }, [timestamp, expenses]);
+    
+    const totalByCategory = useMemo(() => {
+        return Math.round(categories.summary.reduce((acc, category) => {
+            if (category.isIncome) {
+                return acc + category.amount;
+            }
+            return acc - category.amount;
+        }, 0));
+    }, [categories]);
+    
+    const expensesTotalByCategory = useMemo(() => {
+        return Math.round(categories.summary.reduce((acc, category) => {
+            if (category.isIncome) {
+                return acc;
+            }
+             
+            return acc - category.amount;
+        }, 0));
+    }, [categories]);
 
     return {
         totalExpensesThisMonth,
         totalIncomeThisMonth,
+        totalByCategory,
+        expensesTotalByCategory,
     }
 }
